7d3038844deca67f4016709dd67b6d8c26449d26
yen <72980282+YazWare@users.noreply.github.com>
2026-01-04 16:34:06 +0100

    Update shake.js

--- shake.js
+++ shake.js
@@ -1,4 +1,4 @@
-const body = document.body;
+const container = document.getElementById("vhs-container");
 setInterval(() => {
-  body.style.transform = `translate(${Math.random()*2-1}px, ${Math.random()*2-1}px)`;
+  container.style.transform = `translate(${Math.random()*2-1}px, ${Math.random()*2-1}px)`;
 }, 100);
